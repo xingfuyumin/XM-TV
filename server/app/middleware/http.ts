b331e25d04cc9c6faf52b03bef935641db9cf651
xingfuyumin <1459736624@qq.com>
2024-03-04 19:23:20 +0800

    修改运行问题

--- server/app/middleware/http.ts
+++ server/app/middleware/http.ts
@@ -1,33 +1,9 @@
 import { Context } from 'egg';
-const { createProxyMiddleware } = require('http-proxy-middleware');
 
 export default () => {
   return async (ctx: Context, next: () => Promise<any>) => {
     const { url } = ctx.request;
 
-    if (url.startsWith('/api/v1/live/')) {
-      const { liveBase } = ctx.service;
-      let target = '';
-      let path = '';
-      const [id, ts] = url.split('/api/v1/live/')[1]?.split('/');
-      console.log(id, ts,ts?.endsWith('.ts'));
-      if (ts?.endsWith('.ts')) {
-        [target, path] = await liveBase.ts(id, ts);
-      } else {
-        [target, path] = await liveBase.m3u8(id);
-      }
-      // 创建代理中间件
-      const proxy = createProxyMiddleware({
-        target,
-        changeOrigin: true,
-        pathRewrite: {
-          [url]: path,
-        }
-      });
-      ctx.respond = false;
-      proxy(ctx.req, ctx.res, next);
-      return;
-    }
     if (url === '/') {
       ctx.unsafeRedirect('/public/page/index.html');
       return;
