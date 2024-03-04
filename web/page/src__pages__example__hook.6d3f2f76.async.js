8d07de2fd119fb23419f3bfb9af5437aa9619178
周小明 <xmzhou@thinkingdata.cn>
2024-03-04 18:07:25 +0800

    新增页面

---
+++ web/page/src__pages__example__hook.6d3f2f76.async.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk=self.webpackChunk||[]).push([[906],{15727:function(n,u,e){e.r(u),u.default=function(){}}}]);
---
+++ web/page/src__pages__index.f06b015d.async.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk=self.webpackChunk||[]).push([[858],{37025:function(e,u,s){s.r(u);var n=s(1249);u.default=function(){n.history.push("/live")}}}]);
